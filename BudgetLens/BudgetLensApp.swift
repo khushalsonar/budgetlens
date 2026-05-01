0f9a398f53d4f1c87aac06603f98728e763395cb
Khushal <nitinsonar0@gmail.com>
2026-05-01 15:26:28 +0530

    Initial Commit

---
+++ BudgetLens/BudgetLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetLensApp.swift
+//  BudgetLens
+//
+//  Created by Khushal Sonar on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
